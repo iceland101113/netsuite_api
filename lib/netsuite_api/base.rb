1dd37b449c19f0dbffaafe00035ea7a24fefcdd6
hueiyicheng <hueiyicheng@hueiyichengs-MacBook-Pro.local>
2025-03-01 22:31:39 +0800

    update connection and oath signaiture function

--- lib/netsuite_api/base.rb
+++ lib/netsuite_api/base.rb
@@ -31,8 +31,12 @@ module NetsuiteApi
     private
 
     def connection(host)
-      conn = Faraday.new(url: host) do |faraday|
+      Faraday.new(url: host) do |faraday|
+        faraday.request :json # handle JSON payload
+        faraday.response :logger, NetsuiteApi.logger if NetsuiteApi.logger
         faraday.adapter Faraday.default_adapter
+        faraday.options.timeout = 10 # 10 seconds timeout
+        faraday.options.open_timeout = 5 # 5 seconds open timeout
         faraday.ssl.verify = true # You can set this to false if you have SSL verification issues
       end
     end
@@ -42,15 +46,10 @@ module NetsuiteApi
     end
 
     def authorization_header(url, method, query_params, host:)
-      "OAuth realm=\"#{account_id}\",oauth_consumer_key=\"#{consumer_key}\",oauth_token=\"#{token}\",oauth_signature_method=\"HMAC-SHA256\",oauth_timestamp=\"#{oauth_timestamp}\",oauth_nonce=\"#{oauth_nonce}\",oauth_version=\"1.0\",oauth_signature=\"#{generate_oauth_signature(url, method, query_params, host: host)}\""
-    end
+      timestamp = Time.now.to_i
+      nonce = SecureRandom.hex(16)
 
-    def oauth_nonce
-      @oauth_nonce = SecureRandom.hex(16)
-    end
-  
-    def oauth_timestamp
-      @oauth_timestamp = Time.now.to_i
+      "OAuth realm=\"#{account_id}\",oauth_consumer_key=\"#{consumer_key}\",oauth_token=\"#{token}\",oauth_signature_method=\"HMAC-SHA256\",oauth_timestamp=\"#{timestamp}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"#{generate_oauth_signature(url, method, query_params, host: host, timestamp: timestamp, nonce: nonce)}\""
     end
   
     def full_url(url, query_params)
@@ -65,20 +64,21 @@ module NetsuiteApi
       CGI.escape(url)
     end
   
-    def generate_oauth_signature(url, http_method, query_params, host:)
+    def generate_oauth_signature(url, http_method, query_params, host:, timestamp:, nonce:)
       # Step 1: Combine parameters
       all_params = {
         'oauth_consumer_key' => consumer_key,
         'oauth_token' => token,
         'oauth_signature_method' => 'HMAC-SHA256',
-        'oauth_timestamp' => @oauth_timestamp,
-        'oauth_nonce' => @oauth_nonce, # Generate a random nonce
+        'oauth_timestamp' => timestamp,
+        'oauth_nonce' => nonce, # Generate a random nonce
         'oauth_version' => '1.0'
       }
       all_params.merge!(query_params) if query_params
   
       # Step 2: Normalize parameters
-      normalized_params = all_params.sort.map { |k, v| url_encode("#{k}=#{URI.encode_www_form_component(v).gsub('+', '%20')}") }.join('%26') 
+      # normalized_params = all_params.sort.map { |k, v| url_encode("#{k}=#{URI.encode_www_form_component(v).gsub('+', '%20')}") }.join('%26') 
+      normalized_params = URI.encode_www_form(all_params.sort)
   
       # Step 3: Generate Signature Base String (SBS)
       sbs = "#{http_method.to_s.upcase}&#{url_encode("#{host}#{url}")}&#{normalized_params}"
