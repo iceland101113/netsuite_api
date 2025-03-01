52ca3f94b405a7c6ff8deabca663e2aaea7552cd
hueiyicheng <hueiyicheng@hueiyichengs-MacBook-Pro.local>
2025-03-01 22:45:16 +0800

    remove useless line

--- lib/netsuite_api/base.rb
+++ lib/netsuite_api/base.rb
@@ -78,7 +78,6 @@ module NetsuiteApi
   
       # Step 2: Normalize parameters
       normalized_params = all_params.sort.map { |k, v| url_encode("#{k}=#{URI.encode_www_form_component(v).gsub('+', '%20')}") }.join('%26') 
-      # normalized_params = URI.encode_www_form(all_params.sort)
   
       # Step 3: Generate Signature Base String (SBS)
       sbs = "#{http_method.to_s.upcase}&#{url_encode("#{host}#{url}")}&#{normalized_params}"
